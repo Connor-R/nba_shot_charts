d5d6bda717f2446e42f69a2253cb30fb0dd6d792
Connor Reed <connor.reed.92@gmail.com>
2021-04-30 19:02:15 -0700

    add kill statement

--- updaters/nba_shot_chart_posting_run.sh
+++ updaters/nba_shot_chart_posting_run.sh
@@ -8,10 +8,11 @@ HOUR=$(date +"%H")
 
 if { [ "$DOW" -eq "1" ] || [ "$DOW" -eq "3" ] || [ "$DOW" -eq "5" ]; } && [ "$HOUR" == "15" ]; then
 
-    cd ../charting/ && python chart_bot_team.py
+    cd ../charting/ && python chart_bot_team.py & sleep 10m && pkill -f chart_bot_team.py && printf "\n\nkillingbot\n\n"
 
 else
 
-    cd ../charting/ && python chart_bot.py
+    cd ../charting/ && python chart_bot.py & sleep 10m && pkill -f chart_bot.py && printf "\n\nkillingbot\n\n"
 
 fi
+
